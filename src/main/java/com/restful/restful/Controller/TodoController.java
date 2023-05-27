7d75063962d6cda31e79abdd536f094294716222
et860525 <et860525@gmail.com>
2023-05-27 17:18:36 +0800

    Swagger Apply

--- src/main/java/com/restful/restful/Controller/TodoController.java
+++ src/main/java/com/restful/restful/Controller/TodoController.java
@@ -3,6 +3,10 @@ package com.restful.restful.Controller;
 import com.restful.restful.Entity.Todo;
 import com.restful.restful.Service.TodoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,28 +17,34 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name="Todo List")
 public class TodoController {
 	@Autowired
 	TodoService todoService;
 
+	@Operation(summary = "Get all todo", description = "You can get all todos")
 	@GetMapping("/todos")
 	public ResponseEntity<Iterable<Todo>> getTodos() {
 		Iterable<Todo> todoList = todoService.getTodos();
 		return ResponseEntity.status(HttpStatus.OK).body(todoList);
 	}
 
+	@Operation(summary = "Get a todo")
 	@GetMapping("/todos/{id}")
 	public Optional<Todo> getTodo(@PathVariable Integer id) {
 		Optional<Todo> todo = todoService.findById(id);
 		return todo;
 	}
 
+	@Operation(summary = "Create todo")
+	@ApiResponse(responseCode = "201", description = "Create OK")
 	@PostMapping("/todos")
 	public ResponseEntity<Integer> createTodo(@RequestBody Todo todo) {
 		Integer result_todo = todoService.createTodo(todo);
 		return ResponseEntity.status(HttpStatus.CREATED).body(result_todo);
 	}
 
+	@Operation(summary = "Update todo")
 	@PutMapping("/todos/{id}")
 	public ResponseEntity<String> updateTodo(@PathVariable Integer id, @RequestBody Todo todo) {
 		Boolean result_todo = todoService.updateTodo(id, todo);
@@ -44,6 +54,7 @@ public class TodoController {
 		return ResponseEntity.status(HttpStatus.OK).body("");
 	}
 
+	@Operation(summary = "Delete todo")
 	@DeleteMapping("/todos/{id}")
 	public ResponseEntity<String> deletTodo(@PathVariable Integer id) {
 		Boolean result_todo = todoService.deleteTodo(id);
--- src/main/java/com/restful/restful/RestfulApplication.java
+++ src/main/java/com/restful/restful/RestfulApplication.java
@@ -2,6 +2,12 @@ package com.restful.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class RestfulApplication {
@@ -10,4 +16,15 @@ public class RestfulApplication {
 		SpringApplication.run(RestfulApplication.class, args);
 	}
 
+	@Bean
+  public OpenAPI todoOpenAPI() {
+      return new OpenAPI()
+              .info(new Info().title("Todo API")
+              .description("Spring todo application")
+              .version("v0.0.1")
+              .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+              .externalDocs(new ExternalDocumentation()
+              .description("Todo API Documentation")
+              .url("https://springshop.wiki.github.org/docs"));
+  }
 }
